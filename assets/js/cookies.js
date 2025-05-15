5478f3946f7fb7666b7c7c04a168edf2ccbb24f9
pvigjim0502 <156427789+pvigjim0502@users.noreply.github.com>
2025-05-15 19:17:07 +0200

    Corregido archivo de cookies

--- assets/js/cookies.js
+++ assets/js/cookies.js
@@ -20,3 +20,38 @@ function obtenerCookie(nombre) {
 function borrarCookie(nombre) {
     document.cookie = nombre + "=;expires=Thu, 01 Jan 1970 00:00:00 UTC;path=/;samesite=Lax;secure";
 }
+
+function cargarUltimaPagina() {
+    var ultimaPagina = obtenerCookie('ultimaPagina');
+
+    if (ultimaPagina === 'perfil') {
+        document.getElementById('pagina-inicio').style.display = 'none';
+        document.getElementById('contenidoPerfil').style.display = 'block';
+        cargarMiPerfil();
+    } else {
+        document.getElementById('contenidoPerfil').style.display = 'none';
+        document.getElementById('pagina-inicio').style.display = 'block';
+    }
+}
+
+window.onload = function () {
+    var sesionActiva = obtenerCookie('sesionActiva');
+
+    if (sesionActiva === 'true') {
+        document.getElementById('cabecera').style.display = 'block';
+        document.getElementById('formularioLogin').style.display = 'none';
+        document.getElementById('pieDePagina').style.display = 'block';
+
+        var usuarioNombre = obtenerCookie('usuarioNombre');
+        if (usuarioNombre) {
+            document.getElementById('usuario-nombre').textContent = usuarioNombre;
+        }
+
+        cargarUltimaPagina();
+        cargarMenu();
+    } else {
+        document.getElementById('formularioLogin').style.display = 'block';
+        document.getElementById('cabecera').style.display = 'none';
+        document.getElementById('pagina-inicio').style.display = 'none';
+    }
+};
