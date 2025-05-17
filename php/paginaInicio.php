b20c49efdb7c889eb8cfe5a0e7c7edcbd82fedbf
pvigjim0502 <156427789+pvigjim0502@users.noreply.github.com>
2025-05-17 17:15:52 +0200

    Pagina principal, albumnes y artistas

--- php/paginaInicio.php
+++ php/paginaInicio.php
@@ -1,14 +1,24 @@
 <div id="pagina-inicio" class="container-fluid py-5 px-lg-5" style="display: none;">
 
-    <div class="row">
+    <!-- seccion de albumes -->
+    <div class="row d-flex justify-content-center align-items-center">
         <div id="contenedorAlbum" class="row g-4 justify-content-center">
-
+            <!-- aqui iran los albumes cargados -->
         </div>
 
         <div id="contenedorCanciones" class="row" style="display: none;">
             <div class="col-12">
-
+                <!-- aqui iran las canciones cargadas -->
             </div>
         </div>
-    </div>
+
+        <!-- separador -->
+        <hr class="my-5">
+
+        <!-- seccion de artistas -->
+        <div id="contenedorArtistas">
+            <!-- aqui se generaran los slides de los artistas -->
+        </div>
+
+    </div>    
 </div>
