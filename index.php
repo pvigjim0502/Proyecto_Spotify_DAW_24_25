f3faaadde83ecda3389f0c4eeba340c6d7d4e7a2
pvigjim0502 <156427789+pvigjim0502@users.noreply.github.com>
2025-05-17 17:12:28 +0200

    Actualizado del index

--- index.php
+++ index.php
@@ -14,12 +14,12 @@ header("Access-Control-Allow-Headers: Content-Type");
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" rel="stylesheet">
     <link href="assets/css/bootstrap5.3.css" rel="stylesheet">
     <link href="assets/css/estilos.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper/swiper-bundle.min.css">
 </head>
 
 <body>
     <?php
     $rutaBase = __DIR__ . '/php/';
-
     include($rutaBase . 'cabecera.php');
     include($rutaBase . 'login.php');
     include($rutaBase . 'paginaInicio.php');
@@ -32,8 +32,10 @@ header("Access-Control-Allow-Headers: Content-Type");
 
 <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/swiper/swiper-bundle.min.js"></script>
 <script src="/assets/js/funciones.js"></script>
 <script src="/assets/js/foro.js"></script>
 <script src="/assets/js/ajax.js"></script>
+<script src="/assets/js/cookies.js"></script>
 
 </html>
